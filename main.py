641a47f4c533cd8756597aa8bd671c5db9b60d2c
CuriousCoyote <t0aster0ven.jpg@gmail.com>
2026-06-11 00:54:24 -0400

    modularity

--- main.py
+++ main.py
@@ -6,14 +6,28 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = SandwichMaker(resources)
+cashier_instance = Cashier()
 
 
 
 
 def main():
     ###  write the rest of the codes ###
-
+    while True: #loop until input == off
+        order = input("What would you like? \n(small / medium / large / off / report)\nEnter Selection: ").lower()
+        if order == "off":
+            break
+        elif order == "report":
+            print(f"Bread: {sandwich_maker_instance.machine_resources['bread']} slice(s)")
+            print(f"Ham: {sandwich_maker_instance.machine_resources['ham']} slice(s)")
+            print(f"Cheese: {sandwich_maker_instance.machine_resources['cheese']} ounce(s)")
+        elif order in recipes:
+            ingredients = recipes[order]["ingredients"]
+            cost = recipes[order]["cost"]
+            if sandwich_maker_instance.check_resources(ingredients):
+                coins = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(coins, cost):
+                    sandwich_maker_instance.make_sandwich(order, ingredients)
 if __name__=="__main__":
     main()
---
+++ sandwich_maker.py
@@ -0,0 +1,22 @@
+class SandwichMaker:
+
+    #nothing to change here
+    def __init__(self, machine_resources):
+        """Receives resources as input.
+           Hint: bind input variable to self variable"""
+        self.machine_resources = machine_resources
+
+    def check_resources(self, ingredients):
+        """Returns True when order can be made, False if ingredients are insufficient."""
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources[item]:
+                print(f"Sorry, there is not enough {item}.")
+                return False        #if there is not enough of any item, return false
+        return True     #every item has been checked, return true
+
+    def make_sandwich(self, sandwich_size, order_ingredients):
+        """Deduct the required ingredients from the resources.
+           Hint: no output"""
+        for item in order_ingredients:
+            self.machine_resources[item] -= order_ingredients[item]
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
